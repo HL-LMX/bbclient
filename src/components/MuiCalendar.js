708838e78548e972b94d50e6a7fa089283ce2590
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2025-02-07 23:22:01 -0600

    ci: Update calendar component to lock weekends

--- src/components/MuiCalendar.js
+++ src/components/MuiCalendar.js
@@ -1,35 +1,36 @@
 // MuiCalendar.js
-
 import * as React from 'react';
-import dayjs from 'dayjs'; // Import dayjs
+import dayjs from 'dayjs';
+import 'dayjs/locale/en'; // Use the default English locale (week starts on Sunday)
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
 export default function MuiCalendar({ currentDate, onChange }) {
-	// Convert currentDate to proper format using dayjs
-	const formattedCurrentDate = dayjs(currentDate); // Convert to dayjs object
+  // Convert currentDate to a dayjs object
+  const formattedCurrentDate = dayjs(currentDate);
 
-	return (
-		
-		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<DateCalendar
-				value={formattedCurrentDate} // Set value prop to formattedCurrentDate
-				onChange={(newValue) => onChange(newValue.toDate())} // Convert newValue to JavaScript Date
-
-				renderDay={(day, selectedDate, DayComponentProps) => {
-
-				// Check if the day matches the current date
-				if (dayjs(day).format("YYYY-MM-DD") === formattedCurrentDate.format("YYYY-MM-DD")) {
-					// Mark the current date as selected
-					return <DayComponentProps.selected {...DayComponentProps} />;
-				}
-				// Return empty day component
-				return <DayComponentProps.empty {...DayComponentProps} />;
-				}}
-				
-				displayWeekNumber
-			/>
-		</LocalizationProvider>
-	);
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en">
+      <DateCalendar
+        value={formattedCurrentDate}
+        onChange={(newValue) => onChange(newValue.toDate())}
+        // Disable weekend days: Sunday (0) and Saturday (6)
+        shouldDisableDate={(day) => {
+          const dayOfWeek = day.day(); // Sunday: 0, Monday: 1, ... Saturday: 6
+          return dayOfWeek === 0 || dayOfWeek === 6;
+        }}
+        renderDay={(day, selectedDate, DayComponentProps) => {
+          // Highlight the current date if it matches
+          if (
+            dayjs(day).format('YYYY-MM-DD') === formattedCurrentDate.format('YYYY-MM-DD')
+          ) {
+            return <DayComponentProps.selected {...DayComponentProps} />;
+          }
+          return <DayComponentProps.empty {...DayComponentProps} />;
+        }}
+        // Removed displayWeekNumber if it was previously set
+      />
+    </LocalizationProvider>
+  );
 }
