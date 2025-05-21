0375f03fb92d434a27c7570b6f27e955eb4fcf98
shelemhl <146783144+shelemhl@users.noreply.github.com>
2025-05-21 11:01:13 -0600

    ci: Configure frontend for Hamburg server migration

--- src/Variables.js
+++ src/Variables.js
@@ -2,6 +2,7 @@
 
 export const variables={
     // API_URL:"http://127.0.0.1:8000/",
-    API_URL:"https://bookingapi.lenkostudio.com/",
-    
+    // API_URL:"https://bookingapi.lenkostudio.com/",
+    // API_URL: "http://194.9.161.245:81/"
+    API_URL: "/api/"    
 }
