4cb1616e6cd27249d117dfd89d6ca26f22f1384f
Shelem H. <shelemhg@hotmail.com>
2025-05-21 12:28:08 -0600

    ci: Refactor files

--- src/Variables.js
+++ src/Variables.js
@@ -1,8 +1,12 @@
-// client/bookinggite/src/Variables.js
+// src/Variables.js
 
-export const variables={
-    // API_URL:"http://127.0.0.1:8000/",
-    // API_URL:"https://bookingapi.lenkostudio.com/",
+/**
+ * Variables: Central place for application constants.
+ * Update API_URL as needed for different environments.
+ */
+export const variables = {
+    // API_URL: "http://127.0.0.1:8000/",
+    // API_URL: "https://bookingapi.lenkostudio.com/",
     // API_URL: "http://194.9.161.245:81/"
-    API_URL: "/api/"    
-}
+    API_URL: "/api/"
+};
